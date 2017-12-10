c93dba3d1ed96a3cc7d0f397f494995cc222bf9b
Monika Williams <monika.m.williams@gmail.com>
2017-12-10 06:03:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ function BoardMember (name, homeState, training){
     return "I like to help people.";
   }
   this.releasePressStatement = function () {
-    return "You will see great things from Scuber";
+    return "You will see great things from Scuber.";
   }
-  this.veto = function () {
-    return "No, I must disagree";
+  this.sayHi = function () {
+    return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`;
   }
 }
